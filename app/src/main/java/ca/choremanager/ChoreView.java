9ca1c17ced270fd4aab406ce6a632c1be67da66f
MichaelGilhooly <michael.gilhooly1@gmai.com>
2017-11-24 10:29:29 -0500

    - Added Edit Chore Interface - Updated view chore and side navigation menu

--- app/src/main/java/ca/choremanager/ChoreView.java
+++ app/src/main/java/ca/choremanager/ChoreView.java
@@ -1,13 +1,27 @@
 package ca.choremanager;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class ChoreView extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_chore_view);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+
+        inflater.inflate(R.menu.toolbar, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
