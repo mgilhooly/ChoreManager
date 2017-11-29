235874ceb9e108dc9057ce6aacecb280d071c2aa
zurkiyehh <31969322+zurkiyehh@users.noreply.github.com>
2017-11-29 12:04:33 -0500

    .
    null

---
+++ app/src/main/java/ca/choremanager/AddChore.java
@@ -0,0 +1,13 @@
+package ca.choremanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddChore extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_chore);
+    }
+}
