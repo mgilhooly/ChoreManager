b7e0d2f3f2b2b06787b17516f1adf9553ba8de43
MichaelGilhooly <michael.gilhooly1@gmai.com>
2017-11-29 12:32:53 -0500

    Added methods for Chore, User, Family and Tool

--- app/src/main/java/ca/choremanager/Family.java
+++ app/src/main/java/ca/choremanager/Family.java
@@ -9,12 +9,17 @@ import java.util.List;
 
 public class Family {
     private List<User> family;
+    private List<Tool> tools;
 
     public Family (){
         family = Collections.emptyList();
+        tools = Collections.emptyList();
     }
 
     public void addUser(User user){
         family.add(user);
     }
+    public void removeUser(User user){ family.remove(user);}
+    public void addTool (Tool tool) {tools.add(tool);}
+    public void removeTool (Tool tool) {tools.remove(tool);}
 }
